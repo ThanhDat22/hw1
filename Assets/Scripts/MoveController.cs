ba92074d7eadee9377eef2b3058985f54f0ef9be
ThanhDat22 <dat.nguyen2284@gmail.com>
2024-09-23 01:58:47 -0500

    first commit

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEditor;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+    public Vector2 direction;
+    bool running = false;
+
+    float currentTime = 0f;
+    float startingTime = 60f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()   
+    {
+        currentTime -= 1 * Time.deltaTime;
+
+        if (!running)
+        {
+            StartCoroutine(changeDirection());
+        }
+
+        int speedUp = 1;
+
+        // Add twist
+        if (currentTime < 20)
+        {
+            speedUp = 10;
+        }
+
+        rb2d.velocity += new Vector2(direction.x, direction.y) * Time.deltaTime * speed * speedUp;
+
+        
+    }
+
+    IEnumerator changeDirection()
+    {
+        running = true;
+        yield return new WaitForSeconds(1);
+        direction.x = Random.Range(-1, 2);
+        direction.y = Random.Range(-1, 2);
+        running = false;
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+    public Text timerText;
+    public Text winText;
+    public Text gameOverText;
+    public Button restartButton;
+
+    float currentTime = 0f;
+    float startingTime = 60f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+
+        rb2d = GetComponent<Rigidbody2D>();
+
+        gameOverText.text = "";
+
+        winText.text = "";
+        restartButton.gameObject.SetActive(false); // hide button
+    }
+
+    // FixedUpdate is in sysc with physics engine
+    void FixedUpdate()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        timerText.text = "Timer: " + currentTime.ToString("0");
+        if (currentTime < 0)
+        {
+            currentTime = -1;
+        }
+        
+
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        //rb2d.AddForce(movement * speed);
+        rb2d.velocity = movement * speed;
+
+        if (currentTime < 1 && currentTime > 0)
+        {
+            winText.text = "You win!";
+            gameOverText.text = "";
+            restartButton.gameObject.SetActive(true); // show button
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(true); 
+
+            gameOverText.text = "Game Over!";
+
+            currentTime = -1;
+
+            timerText.text = "Timer: ";
+
+            //winText.text = "";
+
+            restartButton.gameObject.SetActive(true); // show button
+        }
+        
+    }
+
+    public void OnRestartButtonPress()
+    {
+        SceneManager.LoadScene("SampleScene"); // restart the game
+    }
+
+}
